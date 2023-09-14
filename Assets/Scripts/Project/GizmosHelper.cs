7e6be41ec700d662c68b980cc4a56dfbd983190a
Eideren <contact@eideren.com>
2023-09-14 14:38:58 +0200

    Project: GizmosHelper

---
+++ Assets/Scripts/Project/GizmosHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class GizmosHelper
+{
+    public static void Label(Vector3 position, string label, Color? color = null)
+    {
+        #if UNITY_EDITOR
+        var previousColor = GUI.color;
+        GUI.color = color ?? previousColor;
+        UnityEditor.Handles.Label(position, label);
+        GUI.color = previousColor;
+        #endif
+    }
+}
