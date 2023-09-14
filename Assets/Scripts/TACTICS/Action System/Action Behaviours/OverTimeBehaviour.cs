d2869e810948709df1a8a3d24c31f4136a2f0ec2
Eideren <contact@eideren.com>
2023-09-14 15:12:57 +0200

    Tactics: Remove deprecated tactics

--- Assets/Scripts/TACTICS/Action System/Action Behaviours/OverTimeBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OverTimeBehaviour : ActionBehaviour
-{
-    // Activate an Effect that occurs over set time
-}
