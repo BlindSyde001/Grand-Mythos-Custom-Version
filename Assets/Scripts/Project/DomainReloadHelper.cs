1d0302783fc36f3eeae21b730b6167b1d3eead32
Eideren <contact@eideren.com>
2023-09-14 14:35:47 +0200

    Project: DomainReloadHelper

---
+++ Assets/Scripts/Project/DomainReloadHelper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Handles rebuilding un-serialized state back after a domain reload, see <see cref="ReloadableBehaviour"/>.
+/// </summary>
+public partial class DomainReloadHelper : MonoBehaviour
+{
+    /// <summary>
+    /// Called before the domain is reloaded, so before un-serialized fields are reset, and before components have their OnEnable() called.
+    /// </summary>
+    public static System.Action<DomainReloadHelper> BeforeReload;
+    /// <summary>
+    /// Called after the domain is reloaded, so after un-serialized fields are reset, but before components have their OnEnable() called.
+    /// </summary>
+    public static System.Action<DomainReloadHelper> AfterReload;
+
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+    static void RunOnStart()
+    {
+        // We need a list of all the reloadables that existed before domain reload, query unity to build one (PreReload),
+        // then store that list into a new game object, finally fetch the gameobject after domain reload (PostReload)
+        // to call the reload function over each item of that list
+        AssemblyReloadEvents.beforeAssemblyReload += PreReload;
+        PostReload();
+    }
+
+    static void PreReload()
+    {
+        if(EditorApplication.isPlaying == false)
+            return;
+
+        var helper = new GameObject(nameof(DomainReloadHelper)).AddComponent<DomainReloadHelper>();
+        BeforeReload?.Invoke(helper);
+    }
+
+    static void PostReload()
+    {
+        var reloadHelper = FindAnyObjectByType<DomainReloadHelper>(FindObjectsInactive.Include);
+        if (reloadHelper is null)
+            return;
+
+        // This occurs before components have their OnEnable() called
+
+        AfterReload?.Invoke(reloadHelper);
+        Destroy(reloadHelper);
+    }
+#endif
+}
---
+++ Assets/Scripts/Project/ReloadableBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles rebuilding un-serialized state back after a domain reload, see <see cref="OnEnabled"/>.
+/// </summary>
+public abstract class ReloadableBehaviour : MonoBehaviour
+{
+    static DomainReloadHelper _reloadHelper;
+    static SerializableHashSet<ReloadableBehaviour> _compLeftToReload = new();
+
+    protected void OnEnable()
+    {
+        bool isRightAfterDomainReload = _compLeftToReload.Remove(this);
+        OnEnabled(isRightAfterDomainReload);
+    }
+
+    protected void OnDisable()
+    {
+        if (_reloadHelper)
+        {
+            _reloadHelper.ReloadableBehaviours.Add(this);
+            OnDisabled(true);
+        }
+        OnDisabled(false);
+    }
+
+    /// <summary>
+    /// Same as <see cref="OnEnable"/>>, so it can also be called after the domain is reloaded in play mode,
+    /// when saving a script while a game is running in the editor for example.
+    /// The parameter should be used when you need to re-initialize stuff that are not serialized, like private or static fields since
+    /// a domain only keeps serialized data.
+    /// </summary>
+    protected abstract void OnEnabled(bool afterDomainReload);
+
+    /// <summary>
+    /// Same as <see cref="OnDisable"/>>, so it can also be called after the domain is reloaded in play mode,
+    /// when saving a script while a game is running in the editor for example.
+    /// The parameter should be used when you need to re-initialize stuff that are not serialized, like private or static fields since
+    /// a domain only keeps serialized data.
+    /// </summary>
+    protected abstract void OnDisabled(bool beforeDomainReload);
+
+    static ReloadableBehaviour()
+    {
+        DomainReloadHelper.BeforeReload += obj => _compLeftToReload = obj.ReloadableBehaviours;
+        DomainReloadHelper.AfterReload += obj => _reloadHelper = obj;
+    }
+}
+
+public partial class DomainReloadHelper
+{
+    public SerializableHashSet<ReloadableBehaviour> ReloadableBehaviours = new();
+}
