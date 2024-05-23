3bd04d0d03ca26f03147e2c2f53c988bb5078d72
Eideren <contact@eideren.com>
2024-05-23 05:51:18 +0200

    Split in game menu into its own prefab

---
+++ Assets/Scripts/INVENTORY/TradableItem.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TradeableItem : BaseItem
+{
+    public int Cost = 10;
+
+    [InfoBox("What happens when the player sells this item")]
+    [SerializeReference]
+    public IInteraction OnPlayerSoldItem;
+}
