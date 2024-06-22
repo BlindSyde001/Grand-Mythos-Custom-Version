462ee7120462b494f390302760602163666ede12
Samuel Jones <samuel.jones@live.com.au>
2024-06-22 21:03:53 +1000

    Readded assets still used in project

---
+++ Assets/Reversed Interactive/NewGen SiFi Slums/Scripts/DayNightToggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightToggle : MonoBehaviour
+{
+
+    public GameObject Day;
+    public GameObject Night;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (Day.activeInHierarchy)
+            {
+                Night.SetActive(true);
+                Day.SetActive(false);
+            }
+            else
+            {
+                Night.SetActive(false);
+                Day.SetActive(true);
+            }
+        }
+    }
+}
