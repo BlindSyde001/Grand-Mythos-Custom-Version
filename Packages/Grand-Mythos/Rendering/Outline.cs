934bf84e28c751792cada954c382d9d329d1c328
Eideren <contact@eideren.com>
2025-05-27 15:37:09 +0200

    Outline feature

---
+++ Packages/Grand-Mythos/Rendering/Outline.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Outline : MonoBehaviour
+{
+    [Required, OnValueChanged(nameof(Apply))] public Material OutlineMaterial;
+    [ReadOnly] public Renderer[] OutlineObjects = Array.Empty<Renderer>();
+
+    private void OnEnable()
+    {
+        Apply();
+    }
+
+    private void OnDisable()
+    {
+        foreach (var o in OutlineObjects)
+            if (o != null)
+                DestroyImmediate(o.gameObject);
+    }
+
+    [Button]
+    private void Apply()
+    {
+        foreach (var o in OutlineObjects)
+            if (o != null)
+                DestroyImmediate(o.gameObject);
+
+        var renderers = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            var renderer = renderers[i];
+            var r = Instantiate(renderer, renderer.transform);
+            r.material = OutlineMaterial;
+            for (int j = r.gameObject.GetComponentCount() - 1; j >= 0; j--)
+            {
+                var c = r.gameObject.GetComponentAtIndex(j);
+                if (c is Transform or Renderer or MeshFilter)
+                    continue;
+                DestroyImmediate(c);
+            }
+
+            if (r is SkinnedMeshRenderer smr)
+            {
+                var m = Instantiate(smr.sharedMesh);
+                AverageNormals(m);
+                smr.sharedMesh = m;
+            }
+
+            r.gameObject.hideFlags |= HideFlags.HideAndDontSave;
+            renderers[i] = r;
+        }
+
+        OutlineObjects = renderers;
+
+        static void AverageNormals(Mesh m)
+        {
+            var n = m.normals;
+            var p = m.vertices;
+
+            var sharedPositions = new Dictionary<Vector3, List<int>>();
+            for (int j = 0; j < p.Length; j++)
+            {
+                if (sharedPositions.TryGetValue(p[j], out var indices) == false)
+                    sharedPositions[p[j]] = indices = new();
+                indices.Add(j);
+            }
+
+            foreach (var (_, indices) in sharedPositions)
+            {
+                var normalAverage = new Vector3();
+                foreach (var index in indices)
+                    normalAverage += n[index];
+                normalAverage.Normalize();
+                foreach (var index in indices)
+                    n[index] = normalAverage;
+            }
+
+            m.normals = n;
+        }
+    }
+}
