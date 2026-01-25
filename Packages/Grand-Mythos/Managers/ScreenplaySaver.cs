f1924a048fad1137117cba0c54f9aac021825434
Eideren <contact@eideren.com>
2026-01-25 13:26:37 +0100

    feat: ScreenplaySaver to enable its state to be reloaded through the saving system

---
+++ Packages/Grand-Mythos/Managers/ScreenplaySaver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Screenplay;
+using Screenplay.Component;
+using UnityEngine;
+
+public class ScreenplaySaver : MonoBehaviour, ISaved<ScreenplaySaver, ScreenplaySaver.HandlerV1>
+{
+    public required ScreenplayDispatcher Dispatcher;
+    
+    public guid UniqueConstID => new("bcfece79-69b0-42da-acb7-751461d39ec5");
+
+    public void Awake()
+    {
+        SavingSystem.TryRestore<ScreenplaySaver, HandlerV1>(this);
+    }
+
+    public void OnDestroy()
+    {
+        SavingSystem.StoreAndUnregister<ScreenplaySaver, HandlerV1>(this);
+    }
+
+    [Serializable] public struct HandlerV1 : ISaveData, ISaveHandler<ScreenplaySaver>
+    {
+        public uint Version => 1;
+
+        public ScreenplayGraph.State State;
+
+        public void Transfer(ScreenplaySaver source, SavingSystem.Transfer transfer)
+        {
+            if (transfer == SavingSystem.Transfer.PullFromSource)
+            {
+                var data = source.Dispatcher.SaveState();
+                State = data;
+            }
+            else
+            {
+                if (source.Dispatcher == null)
+                {
+                    Debug.LogError($"Missing screenplay dispatcher", source);
+                    return;
+                }
+
+                source.Dispatcher.SetStateToLoad(State);
+            }
+        }
+    }
+}
